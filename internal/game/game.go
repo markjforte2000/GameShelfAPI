dcc2e2cba90280191a51e77ee355821b5612ce74
Mark Forte <markjforte2000@gmail.com>
2021-02-14 23:38:51 -0500

    added equals method

--- internal/game/game.go
+++ internal/game/game.go
@@ -15,9 +15,39 @@ type Game struct {
 	Filename          string             `json:"filename"`
 }
 
+func (g *Game) Equal(other *Game) bool {
+	if g.ID != other.ID || g.Filename != other.Filename ||
+		!g.ReleaseDate.Equal(other.ReleaseDate) || g.Summary != other.Summary ||
+		g.Title != other.Title {
+		return false
+	}
+	if !g.Cover.Equal(other.Cover) {
+		return false
+	}
+	for _, genre := range g.Genres {
+		for _, otherGenre := range other.Genres {
+			if !genre.Equal(otherGenre) {
+				return false
+			}
+		}
+	}
+	for _, company := range g.InvolvedCompanies {
+		for _, otherCompany := range other.InvolvedCompanies {
+			if !company.Equal(otherCompany) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 type Artwork struct {
 	RemoteURL string `json:"url"`
 	ID        int    `json:"id"`
+}
+
+func (art *Artwork) Equal(other *Artwork) bool {
+	return art.ID == other.ID && art.RemoteURL == other.RemoteURL
 }
 
 type Genre struct {
@@ -25,11 +55,20 @@ type Genre struct {
 	ID   int    `json:"id" db:"id"`
 }
 
+func (genre *Genre) Equal(other *Genre) bool {
+	return genre.ID == other.ID && genre.Name == other.Name
+}
+
 type InvolvedCompany struct {
 	Name      string `json:"name"`
 	ID        int    `json:"id"`
 	Publisher bool   `json:"publisher"`
 	Developer bool   `json:"developer"`
+}
+
+func (company *InvolvedCompany) Equal(other *InvolvedCompany) bool {
+	return company.Developer == other.Developer && company.Publisher == other.Publisher &&
+		company.ID == other.ID && company.Name == other.Name
 }
 
 type Platform struct {
